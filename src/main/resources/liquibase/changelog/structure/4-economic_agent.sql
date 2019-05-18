fc8739a9a694c12b9c447459508a1851bdf69eef
Sergey Didenko <sergey.didenko.dev@gmail.com>
2019-05-19 01:30:34 +0300

    fixed sql

--- src/main/resources/liquibase/changelog/structure/4-economic_agent.sql
+++ src/main/resources/liquibase/changelog/structure/4-economic_agent.sql
@@ -4,13 +4,12 @@
 CREATE TABLE `economic_agent`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `owner_id` bigint(20) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `addr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `fax` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `ea_oi`(`owner_id`) USING BTREE,
+  `name` varchar(255) NOT NULL,
+  `addr` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `fax` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
   CONSTRAINT `ea_oi` FOREIGN KEY (`owner_id`) REFERENCES `economic_sector` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = MyISAM AUTO_INCREMENT = 1;
 --rollback drop table economic_agent;
